5e3eb3068a39e904dcfabf97bbe86578a7f27e0c
Степан Крапивин <xevinbox@yandex.ru>
2022-04-09 13:37:27 +0300

    #2 можно поставить бомбу; отрисовка лучей взрыва; анимация взрыва

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const TILE_W = 32
+export const TILE_H = 32
+
+export const FrameConfig = {
+  frameWidth: TILE_W,
+  frameHeight: TILE_H
+}
--- src/game.scene.js
+++ src/game.scene.js
@@ -1,6 +1,11 @@
+import { TILE_W, TILE_H, FrameConfig } from "./constants"
+
 export class GameScene extends Phaser.Scene {
+  fireKey = null
   cusrors = null
   player = null
+  bombs = null
+  blasts = []
   playerSpeed = 120
   isHMoves = false // движение только по горизонтали
   isVMoves = false // движение только по вертикали
@@ -10,18 +15,12 @@ export class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet("dude", "assets/dude.png", {
-      frameWidth: 32,
-      frameHeight: 32
-    })
+    this.load.spritesheet("dude", "assets/dude.png", FrameConfig)
+    this.load.spritesheet("bomb", "assets/bomb.png", FrameConfig)
+    this.load.spritesheet("blast", "assets/blast-frames.png", FrameConfig)
   }
 
-  create() {
-    this.player = this.physics.add.sprite(120, 120, "dude");
-    this.player.setBounce(0)
-    this.player.setCollideWorldBounds(true)
-    this.cursors = this.input.keyboard.createCursorKeys()
-
+  createAnimations() {
     this.anims.create({
       key: "idle",
       frames: [{key: "dude", frame: 0}],
@@ -36,10 +35,125 @@ export class GameScene extends Phaser.Scene {
       repeat: -1
     })
 
-    this.player.anims.play("idle")
+    this.anims.create({
+      key: "bomb",
+      frames: this.anims.generateFrameNumbers("bomb", {start: 0, end: 3}),
+      frameRate: 1.2,
+      repeat: 0
+    })
+
+    this.anims.create({
+      key: "blast-cross",
+      frames: [
+        { key: "blast", frame: 0 },
+        { key: "blast", frame: 3 },
+        { key: "blast", frame: 6 },
+        { key: "blast", frame: 3 },
+        { key: "blast", frame: 0 },
+      ],
+      frameRate: 6,
+      repeat: 0
+    })
+
+    this.anims.create({
+      key: "blast-body",
+      frames: [
+        { key: "blast", frame: 1 },
+        { key: "blast", frame: 4 },
+        { key: "blast", frame: 7 },
+        { key: "blast", frame: 4 },
+        { key: "blast", frame: 1 },
+      ],
+      frameRate: 6,
+      repeat: 0
+    })
+
+    this.anims.create({
+      key: "blast-tail",
+      frames: [
+        { key: "blast", frame: 2 },
+        { key: "blast", frame: 5 },
+        { key: "blast", frame: 8 },
+        { key: "blast", frame: 5 },
+        { key: "blast", frame: 2 },
+      ],
+      frameRate: 6,
+      repeat: 0
+    })
   }
 
-  update() {
+  create() {
+    this.player = this.physics.add.sprite(
+      TILE_W*4,
+      TILE_H*8,
+      "dude"
+    )
+
+    this.player.setBounce(0)
+    this.player.setCollideWorldBounds(true)
+    this.player.availableBombCount = 2
+    this.player.blastSize = 2 // длина взрыва: счёт от центра (перекрестье)
+
+    this.bombs = this.physics.add.staticGroup()
+
+    this.physics.add.collider(this.player, this.bombs)
+
+    this.cursors = this.input.keyboard.createCursorKeys()
+    this.fireKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+
+    this.createAnimations()
+  }
+
+  spawnBomb(placeTime, position) {
+    const newBomb = this.bombs.create(
+      position.x,
+      position.y,
+      "bomb"
+    )
+    newBomb.anims.play("bomb")
+    newBomb.startTime = placeTime
+  }
+
+  drawBlast(position, size, currentTime) {
+    const crossBlast = this.physics.add.sprite(
+      position.x,
+      position.y,
+      "blast",
+    )
+
+    crossBlast.startTime = currentTime
+    crossBlast.anims.play("blast-cross")
+    this.blasts.push(crossBlast)
+
+    const dirs = [
+      Phaser.Math.Vector2.LEFT,
+      Phaser.Math.Vector2.RIGHT,
+      Phaser.Math.Vector2.UP,
+      Phaser.Math.Vector2.DOWN,
+    ]
+
+    for(let i=1; i <= size; i++) {
+      dirs.forEach(dir => {
+        const x = position.x + (dir.x * TILE_W * (i))
+        const y = position.y + (dir.y * TILE_W * (i))
+        const newBlast = this.physics.add.sprite(x, y)
+
+        if (dir === Phaser.Math.Vector2.UP) {
+          newBlast.body.rotation = 270
+        } else if (dir === Phaser.Math.Vector2.DOWN) {
+          newBlast.body.rotation = 90
+        } else if (i === size && dir === Phaser.Math.Vector2.LEFT) {
+          newBlast.body.rotation = 180
+        }
+
+        newBlast.anims.play(i === size ? "blast-tail" : "blast-body")
+        newBlast.startTime = currentTime
+        this.blasts.push(newBlast)
+      })
+    }
+  }
+
+  update(time, delta) {
     if (!this.isVMoves) {
       if (this.cursors.left.isDown) {
         this.isHMoves = true
@@ -75,5 +189,44 @@ export class GameScene extends Phaser.Scene {
         this.player.setVelocityY(0)
       }
     }
+
+    if (Phaser.Input.Keyboard.JustDown(this.fireKey)) {
+      if (this.player.availableBombCount) {
+        // забираем у игрока одну бомбу
+        this.player.availableBombCount -= 1
+        // и ставим её с округлёнными координатами - для привязки к ближайшему тайлу
+        this.spawnBomb(time, {
+          x: Math.floor((this.player.x) / TILE_W) * TILE_W + 16,
+          y: Math.floor((this.player.y) / TILE_W) * TILE_W + 16
+        })
+      }
+    }
+
+    // удаляем старые взрывы
+    this.blasts.forEach(blast => {
+      if (time - blast.startTime > 1000) {
+        blast.destroy()
+      }
+    })
+
+    this.bombs.children.entries.forEach(bomb => {
+      if (time - bomb.startTime > 3000) {
+        // начинаем взрыв
+        this.drawBlast(
+          {
+            x: bomb.x,
+            y: bomb.y
+          },
+          this.player.blastSize,
+          time
+        )
+
+        // бомба отработала и больше не нужна
+        bomb.destroy()
+
+        // возвращаем игроку возможность ставить бомбу
+        this.player.availableBombCount += 1
+      }
+    })
   }
 }
