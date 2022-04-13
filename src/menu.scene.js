07fb13c367c3f034e5b521a4d4b63f6bdfb7dda7
Павел Тарадин <it.taradin@gmail.com>
2022-04-13 22:06:45 +0300

    добавлена логика переключения пунктов меню

--- src/menu.scene.js
+++ src/menu.scene.js
@@ -1,5 +1,8 @@
 export class MenuScene extends Phaser.Scene {
 
+  bomb = null
+  cursors = null
+
   config = {
     startX: 160,
     startY: 200,
@@ -12,78 +15,78 @@ export class MenuScene extends Phaser.Scene {
     }
   }
   
-  menuItems = {
-
-    newGame: {
+  menuItems = [
+    {
+      name: 'newGame',
       text: 'Новая игра',
       position: 1,
       current: true,
     },
-
-    options: {
+    {
+      name: 'options',
       text: 'Опции',
       position: 2,
       current: false,
     },
-
-    about: {
+    {
+      name: 'about',
       text: 'Об игре',
       position: 3,
       current: false,
     }
-  }
-
-  bomb = null
-  cursors = null
+  ]
 
   constructor() {
     super("MenuScene")
   }
 
   preload() {
-    this.load.image("menu-bomb", "assets/bomb.png")
+    this.load.image("menu-bomb", "assets/menu_assets/bomb.png")
   }
+
   create() {
-    //this.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
-    //this.input.keyboard.addKeys('LEFT, RIGHT, UP, DOWN, ENTER')
     this.bomb = this.add.image(130, 205,"menu-bomb")
     this.bomb.setDisplaySize(30, 30)
+    this.cursors = this.input.keyboard.createCursorKeys()
 
+    //* вывожу пункты меню в сцену
+    this.menuItems.forEach( (item, index) => {
+      item.textObj = this.add.text(this.config.startX, index ? this.config.lastY + this.config.incrementY : this.config.startY, item.text, this.config.style)
+      this.config.lastY = item.textObj.y
+      
+    })
 
-    //console.log('hello')
-    for (const item in this.menuItems) {
-
-        Object.assign(this.menuItems[item], this.add.text(this.config.startX, this.config.lastY ? this.config.lastY + this.config.incrementY : this.config.startY, this.menuItems[item].text, this.config.style))
-        this.config.lastY = this.menuItems[item].y
-        console.log(this.menuItems[item])
-    }
-
-    //Object.assign(this.menuItems.newGame, this.add.text(160, 200, this.menuItems.newGame.text, this.config.style))
-    //Object.assign(this.menuItems.options, this.add.text(160, 240, this.menuItems.options.text, this.config.style))
-
-
-  }
-
-  update() {
     this.input.keyboard.on('keydown-ENTER', () => {
-      //console.log('hello')
-      this.scene.sleep("MenuScene")
       this.scene.start("GameScene")
     })
 
+  }
 
-    // if (this.cursors.down.isDown) {
-    //   this.bomb.setY(this.menuItems.options.y + 5)
-    //   }
-    //   console.log(Object.keys(this.menuItems))
-    //
-    // if (this.cursors.up.isDown) {
-    //   this.bomb.setY(this.menuItems.newGame.y + 5)
-    // }
-    //
-    // if (this.cursors..isDown) {
-    //
-    // }
+  update(time, delta) {
+    let currentItemIndex = this.menuItems.findIndex((item, index, arr) => item.current)
+    if (this.cursors.up.isDown) {
+      if (currentItemIndex === 0) {
+        this.bomb.setY(this.menuItems[this.menuItems.length - 1].textObj.y + 5)
+        this.menuItems[this.menuItems.length - 1].current = true
+
+      } else {
+
+        this.bomb.setY(this.menuItems[currentItemIndex - 1].textObj.y + 5)
+        this.menuItems[currentItemIndex - 1].current = true
+      }
+      this.menuItems[currentItemIndex].current = false
+    } else if (this.cursors.down.isDown) {
+      if (currentItemIndex === this.menuItems.length - 1) {
+        this.bomb.setY(this.menuItems[0].textObj.y + 5)
+        this.menuItems[0].current = true
+
+      } else {
+
+        this.bomb.setY(this.menuItems[currentItemIndex + 1].textObj.y + 5)
+        this.menuItems[currentItemIndex + 1].current = true
+      }
+      this.menuItems[currentItemIndex].current = false
+    }
 
   }
 }
