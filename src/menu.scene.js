9830f92f8bff4483c957ffe7ae6f30e1b2db132c
Павел Тарадин <it.taradin@gmail.com>
2022-04-14 20:50:37 +0300

    пофиксил меню

--- src/menu.scene.js
+++ src/menu.scene.js
@@ -1,7 +1,7 @@
 export class MenuScene extends Phaser.Scene {
 
   bomb = null
-  cursors = null
+  currentItemIndex = 0
 
   config = {
     startX: 160,
@@ -9,9 +9,9 @@ export class MenuScene extends Phaser.Scene {
     lastY: null,
     incrementY: 40,
     style: {
-      fontSize: 20,
+      fontSize: 24,
       fontFamily: "monospace",
-      color: "#ff6d01"
+      color: "#b94200"
     }
   }
   
@@ -52,6 +52,7 @@ export class MenuScene extends Phaser.Scene {
     //* вывожу пункты меню в сцену
     this.menuItems.forEach( (item, index) => {
       item.textObj = this.add.text(this.config.startX, index ? this.config.lastY + this.config.incrementY : this.config.startY, item.text, this.config.style)
+      item.textObj.setShadow(2,3,'rgb(30,30,30)',1,true,true)
       this.config.lastY = item.textObj.y
       
     })
@@ -60,33 +61,27 @@ export class MenuScene extends Phaser.Scene {
       this.scene.start("GameScene")
     })
 
+    this.input.keyboard.on('keydown-UP', () => {
+      this.currentItemIndex = this.menuItems.findIndex((item) => item.current)
+      this.menuItems[this.currentItemIndex].current = false
+
+      this.currentItemIndex === 0 ? this.currentItemIndex = this.menuItems.length - 1 : this.currentItemIndex--
+
+      this.menuItems[this.currentItemIndex].current = true
+    })
+
+    this.input.keyboard.on('keydown-DOWN', () => {
+      this.currentItemIndex = this.menuItems.findIndex((item, index, arr) => item.current)
+      this.menuItems[this.currentItemIndex].current = false
+
+      this.currentItemIndex === this.menuItems.length - 1 ? this.currentItemIndex = 0 : this.currentItemIndex++
+      this.menuItems[this.currentItemIndex].current = true
+
+    })
+
   }
 
-  update(time, delta) {
-    let currentItemIndex = this.menuItems.findIndex((item, index, arr) => item.current)
-    if (this.cursors.up.isDown) {
-      if (currentItemIndex === 0) {
-        this.bomb.setY(this.menuItems[this.menuItems.length - 1].textObj.y + 5)
-        this.menuItems[this.menuItems.length - 1].current = true
-
-      } else {
-
-        this.bomb.setY(this.menuItems[currentItemIndex - 1].textObj.y + 5)
-        this.menuItems[currentItemIndex - 1].current = true
-      }
-      this.menuItems[currentItemIndex].current = false
-    } else if (this.cursors.down.isDown) {
-      if (currentItemIndex === this.menuItems.length - 1) {
-        this.bomb.setY(this.menuItems[0].textObj.y + 5)
-        this.menuItems[0].current = true
-
-      } else {
-
-        this.bomb.setY(this.menuItems[currentItemIndex + 1].textObj.y + 5)
-        this.menuItems[currentItemIndex + 1].current = true
-      }
-      this.menuItems[currentItemIndex].current = false
-    }
-
+  update() {
+    this.bomb.setY(this.menuItems[this.currentItemIndex].textObj.y + 5)
   }
 }
